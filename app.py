c0b5402a7727ba5d7260047c93ee2200ef234971
moostofa <mmdrahinz96@gmail.com>
2021-10-25 19:52:44 +1100

    repo init

---
+++ app.py
@@ -0,0 +1,186 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+    #gets the details of user in current session
+    #db.execute returns a list with 1 dict. That dict holds current user's details, so get it
+    current_user = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]    
+    return render_template("index.html", user = current_user, jsonified = jsonify(current_user))
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    if request.method == "POST":
+        return
+    else:
+        return render_template("buy.html")
+    """Buy shares of stock"""
+
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    return apology("TODO")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "POST":        #when user fills in and submits the form
+        #get ticker symbol
+        symbol = request.form.get("symbol")
+        if not symbol:
+            return apology("please enter a symbol", 403)
+            
+        #look up ticker symbol and get quote
+        quote = lookup(symbol)
+        if not quote:
+            return apology("ticker symbol does not exist", 403)
+        
+        return render_template("quoted.html", stock_info = quote)
+    else:       #else: GET request for quote.html
+        return render_template("quote.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    #consider doing this part in javascript
+    #get a list of existing usernames
+    existing_usernames = db.execute("SELECT username FROM users")
+    
+    #if the form is submitted
+    if request.method == "POST":
+        #check for valid username
+        name = request.form.get("username")
+        if not name:
+            return apology("must provide username", 403)
+        if name in existing_usernames:
+            return apology("username already exists", 403)
+            
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm-password")
+        
+        #check for valid password
+        if not password: 
+            return apology("please provide a password", 403)
+        if password != confirm_password:
+            return apology("passwords do not match", 403)
+            
+        #add new user to database
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", name, generate_password_hash(password))
+        return redirect("/")
+    else:       
+        return render_template("register.html")     #GET request: display register.html to user     
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+    return apology("TODO")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
